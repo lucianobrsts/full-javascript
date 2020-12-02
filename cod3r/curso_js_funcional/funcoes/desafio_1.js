bcff56ef05ef0efdbd84d060f1372d192fe59b90
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-02 19:46:05 -0300

    Iniciado resolucao do desafio

--- cod3r/curso_js_funcional/funcoes/desafio_1.js
+++ cod3r/curso_js_funcional/funcoes/desafio_1.js
@@ -7,3 +7,20 @@ function somar(a) {
         }
     }
 }
+
+const somarAB = somar(3)(4)
+console.log(somarAB(13))
+console.log(somar(13)(20)(30))
+
+// fn -> 3 * 7
+// fn -> 3 + 7
+// fn -> 3 - 7
+// calcular(3)(7)(fn)
+
+function calcular(x) {
+    return function(y) {
+        return function(fn) {
+            return fn(x, y)
+        }
+    }
+}
