48047377c2089c3690a06dd6a1a55b91e76e8e7b
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-02 19:00:16 -0300

    Flexibilidade com parametros

---
+++ cod3r/curso_js_funcional/funcoes/funcao_1.js
@@ -0,0 +1,29 @@
+let a = 4
+console.log(a)
+
+//Function Declaration
+function bomDia() {
+    console.log('Bom dia!')
+}
+
+bomDia()
+
+// Function Expression
+const boaTarde = function () {
+    console.log('Boa Tarde')
+}
+
+boaTarde()
+
+function somar(a, b) {
+    return a + b
+}
+
+let resultado = somar(3, 4)
+console.log(resultado)
+
+resultado = somar(3, 5, 5, 6, 7, 8)
+console.log(resultado)
+
+resultado = somar(3)
+console.log(resultado)
