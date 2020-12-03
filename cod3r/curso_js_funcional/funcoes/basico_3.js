788a88524e6cab735db681c8eda9be05bb23ca22
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-03 19:18:52 -0300

    This e sua relevancia

--- cod3r/curso_js_funcional/funcoes/basico_3.js
+++ cod3r/curso_js_funcional/funcoes/basico_3.js
@@ -13,6 +13,9 @@ const somar = (...numeros) => {
     }
     return total
 }
+const subtrair = (a, b) => a - b
+
+console.log(subtrair(4, 1))
 
 console.log(somar(1, 2, 3))
 console.log(somar(1, 2, 3, 4, 5, 6))
@@ -20,3 +23,16 @@ console.log(somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
 const potencia = base => exp => Math.pow(base, exp)
 console.log(potencia(2)(10))
+
+//this
+Array.prototype.ultimo = function() {
+    console.log(this[this.length - 1])
+}
+
+Array.prototype.primeiro = function() {
+    console.log(this[0])
+}
+
+const numeros = [-333, 1, 2, 3, 500]
+numeros.ultimo()
+numeros.primeiro()
