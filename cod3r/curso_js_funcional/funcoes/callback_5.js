bc818ea88f6a9b9ee5dcee389af3b478d9abde94
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-16 04:37:09 -0300

    Funcao reduce criada manualmente

--- cod3r/curso_js_funcional/funcoes/callback_5.js
+++ cod3r/curso_js_funcional/funcoes/callback_5.js
@@ -14,3 +14,22 @@ const totalGeral = carrinho
     .reduce(somar)
 
     console.log(totalGeral)
+
+    Array.prototype.meuReduce = function(fn, inicial) {
+        let acc = inicial
+
+        for(let i = 0; i < this.length; i++) {
+          if(!acc && i === 0) {
+              acc = this[i]
+              continue
+          }  
+          acc = fn(acc, this[i], i, this)
+        }
+        return acc
+    }
+
+    const totalGeral2 = carrinho
+        .map(getTotal)
+        .meuReduce(somar)
+
+console.log(totalGeral2)
