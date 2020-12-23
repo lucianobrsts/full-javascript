712461292e7f2fab925d605fca4dd89f7931ae03
lucianobrsts <lucianobrsts@yahoo.com.br>
2020-12-23 10:37:35 -0300

    Modificado estrutura do arquivo

--- cod3r/curso_js_funcional/funcoes/promise_3.js
+++ cod3r/curso_js_funcional/funcoes/promise_3.js
@@ -1,6 +1,5 @@
 function gerarNumerosEntre(min, max) {
     if(min > max) [max, min] = [min, max]
-
     return new Promise(resolve => {
         const fator = max - min + 1
         const aleatorio = parseInt(Math.random() * fator) + min
