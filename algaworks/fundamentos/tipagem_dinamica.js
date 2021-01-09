3e50c43d311b528256ae3e844037446fc2c6f588
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-09 07:40:47 -0300

    Tipagem dinamica

---
+++ algaworks/fundamentos/tipagem_dinamica.js
@@ -0,0 +1,13 @@
+var idade = 25;
+console.log(typeof idade);
+console.log(idade);
+
+idade += ' anos';
+console.log(typeof idade);
+console.log(idade);
+
+idade = false;
+console.log(typeof idade);
+console.log(idade)
+
+// Tipagem dinâmica é quando o tipo das variaveis muda de acordo com as atribuições
