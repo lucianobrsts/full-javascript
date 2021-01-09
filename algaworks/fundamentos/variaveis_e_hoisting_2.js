c03b6a76ef55501dcd8fbb5ae3475c66d9bdbbdc
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-09 07:32:49 -0300

    Modificada estrutura do curso

---
+++ algaworks/fundamentos/variaveis_e_hoisting_2.js
@@ -0,0 +1,19 @@
+var percentualImposto = 0.15;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar: ', totalImposto);
+
+valor = 100;
+
+//Hoisting
+
+/*
+var percentualImposto, totalImposto, valor;
+percentualImposto = 0.15;
+totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+
+valor = 100
+
+*/
