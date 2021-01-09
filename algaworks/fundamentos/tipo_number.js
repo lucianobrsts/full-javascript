ccbdb36820a215a7fbef543ef3744a10959c5966
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-09 07:56:43 -0300

    Tipo number

---
+++ algaworks/fundamentos/tipo_number.js
@@ -0,0 +1,26 @@
+var x = 10;
+console.log(typeof x);
+
+var y = 0.1;
+console.log(typeof y);
+
+var k = 10;
+console.log(y.toString(2));
+console.log(y.toString(16));
+
+var i = 10 / 0;
+console.log(i);
+console.log(typeof i);
+
+var i = -10 / 0;
+console.log(i);
+console.log(typeof i);
+
+var z = 10 / 'Algaworks';
+console.log(z);
+console.log(typeof z);
+console.log(isNaN(z));
+
+var b = 10 / '2';
+console.log(b);
+console.log(typeof b);
