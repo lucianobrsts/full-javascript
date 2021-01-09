19515964967afc6a53c8763666ff8b53971a11e8
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-09 07:26:46 -0300

    Variaveis e hoisting continuação

--- algaworks/variaveis_e_hoisting.js
+++ algaworks/variaveis_e_hoisting.js
@@ -1,5 +1,20 @@
 var percentualImposto = 0.15;
-var valor = 100;
+valor = 100;
 var totalImposto = valor * percentualImposto;
 
 console.log('Total imposto a pagar: ', totalImposto);
+
+var valor;
+
+//HOISTING
+//Leva toda declaração de variável para o inicio das declarações... Começo do código
+
+/*
+Segue código acima após ser aplicado o Hoisting
+var percentualImposto, valor, totalImposto; 
+percentualImposto = 0.15;
+valor = 100;
+totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto)
+*/
