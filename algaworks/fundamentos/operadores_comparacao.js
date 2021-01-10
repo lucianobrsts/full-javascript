6dc8d8e7ac87b42f37dbc96a5d6505e48842e61c
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-10 10:06:06 -0300

    Oepradores de comparacao

---
+++ algaworks/fundamentos/operadores_comparacao.js
@@ -0,0 +1,25 @@
+console.log(10 > 4);
+
+console.log(10 < 4);
+
+console.log(10 > 10);
+
+console.log(10 >= 10);
+
+console.log(10 <= 10);
+
+console.log(10 == 10);
+
+console.log('Algaworks' == 'Algaworks');
+
+var x = 10;
+console.log(typeof x);
+
+var y = '10';
+console.log(typeof y);
+
+console.log(x == y);
+console.log(x === y);
+
+console.log(10 != '10');
+console.log(10 !== '10');
