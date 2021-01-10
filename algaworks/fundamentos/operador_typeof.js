419777fc8165978e49cec0dc07eab6121942cfad
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-10 09:49:40 -0300

    Operador typeof

---
+++ algaworks/fundamentos/operador_typeof.js
@@ -0,0 +1,23 @@
+var m = 'Algaworks';
+console.log(typeof m);
+
+var ativo = true;
+console.log(typeof ativo);
+
+var obj = {};
+console.log(typeof obj);
+
+var x = 10;
+console.log(typeof x);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+var array = [];
+console.log(typeof array);
+
+var f = function() {};
+console.log(typeof f);
+
+
+
