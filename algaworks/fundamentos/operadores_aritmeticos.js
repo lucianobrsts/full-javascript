011bc02aefc4f168a52e0a0b3b35510918a6d371
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-10 09:55:11 -0300

    Operadores Aritmeticos

---
+++ algaworks/fundamentos/operadores_aritmeticos.js
@@ -0,0 +1,20 @@
+console.log('--------------------------------------');
+var r = 4 + 3;
+console.log('A soma é: ' + r);
+console.log('--------------------------------------');
+
+r = 4 - 3;
+console.log('A subtração é: ' + r);
+console.log('--------------------------------------');
+
+r = 4 * 3;
+console.log('A multiplicação é: ' + r);
+console.log('--------------------------------------');
+
+r = 10 / 2;
+console.log('A divisão é: ' + r);
+console.log('--------------------------------------');
+
+r = 7 % 2;
+console.log('O resto da divisão é: ' + r);
+console.log('--------------------------------------');
