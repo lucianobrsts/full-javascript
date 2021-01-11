04c8d8a8449390c861a69f6cbf4268b7e77d2313
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-11 10:21:27 -0300

    Estrutura de controle if/else 2

---
+++ algaworks/fundamentos/estrutura_de_controle_if_else_2.js
@@ -0,0 +1,10 @@
+var valor = 10;
+
+if(!valor) {
+    console.log('Valor deve ser maior que zero!')
+}
+
+var idade = 5;
+if(idade) {
+    console.log('Daqui a 5 anos você terá', idade + 5, 'anos');
+}
