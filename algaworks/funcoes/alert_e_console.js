dfd868dce40a1e9e5a7d19d51c970da91fbfc8f8
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-12 09:57:01 -0300

    Alert e console.log

---
+++ algaworks/funcoes/alert_e_console.js
@@ -0,0 +1,8 @@
+//Retorna um erro pois não está definido, funciona no console do browser
+//alert('Olá tudo bem...');
+
+//funciona normal pois executa no console da IDE ou editor
+console.log('Olá tudo bem...');
+
+//só se executa uma função abrindo e fechando parenteses
+console.log
