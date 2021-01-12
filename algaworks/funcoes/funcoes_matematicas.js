577e9eae6a0881834eb55433ca4c3f31ab28cf4d
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-12 10:30:41 -0300

    Funcoes matematicas

---
+++ algaworks/funcoes/funcoes_matematicas.js
@@ -0,0 +1,24 @@
+ //menor numero informado
+ var menorNota = Math.min(3, 5, 1, 6, 10);
+ console.log('Menor nota', menorNota);
+
+  //maior numero informado
+  var maiorNota = Math.max(3, 5, 1, 6, 10);
+  console.log('Maior nota', maiorNota);
+
+
+   //arredonda numero informado
+   var arredonda = Math.round(22.7);
+   console.log('Arredonda valor: ', arredonda);
+
+   //gera valores aleatórios
+   var valorAleatorio = Math.random();
+   console.log('Valor aleatório: ', valorAleatorio);
+
+   //gera valores aleatórios
+   var valorAleatorio = Math.random();
+   console.log('Valor aleatório: ', valorAleatorio);
+   var numero = valorAleatorio * 60;
+   console.log('número', numero);
+   var numeroMega = Math.floor(numero);
+   console.log('número da mega', numeroMega);
