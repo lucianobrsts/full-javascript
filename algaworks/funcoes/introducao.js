de7def6d1c588b20f5ff069440f9aef3fe924985
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-12 09:48:42 -0300

    Introducao a funcoes

---
+++ algaworks/funcoes/introducao.js
@@ -0,0 +1,11 @@
+//Sintaxe básica de uma função JavaScript
+
+function nome(args) {
+    return;
+}
+
+//codigo mais legível
+//recursividade
+//encapsulamento de codigos
+//reaproveitamento de codigos
+
