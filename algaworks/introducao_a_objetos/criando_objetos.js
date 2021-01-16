89c9c3475840037f363f4f54b25af768eeb73970
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-16 15:51:21 -0300

    Criando objetos

---
+++ algaworks/introducao_a_objetos/criando_objetos.js
@@ -0,0 +1,33 @@
+var fox = { cor: 'prata', modelo: 'Fox', fabricante: 'VW' }
+
+console.log(fox);
+console.log(typeof fox);
+
+console.log('Cor do Fox: ', fox.cor);
+//pintando o fox
+fox.cor = 'Branco';
+console.log('Fox depois de pintando', fox);
+console.log('Modelo: ', fox['modelo']);
+
+fox.peso = 1100;
+console.log(fox);
+
+console.log('Potência', fox.potencia);
+
+fox.ligar = function() { 
+    console.log('Ligando o carro!'); 
+};
+
+console.log(fox);
+fox.ligar();
+
+var celta = {
+    cor: 'branco',
+    modelo: 'celta',
+    fabricante: 'GM',
+    ligar: function() {
+        console.log('Ligando o carro');
+    }
+};
+
+console.log('celta', celta)
