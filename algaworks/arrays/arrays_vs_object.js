7aba885f0deb456806dad5240dcde01f26cb36c5
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-17 11:19:54 -0300

    Arrays vc Objetct

---
+++ algaworks/arrays/arrays_vs_object.js
@@ -0,0 +1,17 @@
+var foxArray = ['Fox', 'Prata', 'VW'];
+
+console.log(typeof foxArray);
+console.log(foxArray[0]);
+console.log(foxArray[1]);
+console.log(foxArray[2]);
+console.log('---------------------------');
+
+var foxObj = {modelo: 'Fox', cor: 'Prata', fabricante: 'VW'}
+console.log(typeof foxObj);
+console.log(foxObj.modelo);
+console.log(foxObj.cor);
+console.log(foxObj.fabricante);
+console.log('---------------------------');
+
+var emails = ['joao@emails.com', 'maria@emails.com'];
+console.log(emails);
