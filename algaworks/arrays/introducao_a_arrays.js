cc8100de23fac6782441d688181358ce51586f4d
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-17 11:03:09 -0300

    Introducao a arrays

---
+++ algaworks/arrays/introducao_a_arrays.js
@@ -0,0 +1,5 @@
+//Arrarys em javascript não tem limite de tamanho ou elementos. Diferente do java
+// que não tem seu tamanho dinâmico.
+
+//O typeof de um array em javascript retorna um object pois o mesmo tem elemento normai
+// sendo acessados através de um índice.
