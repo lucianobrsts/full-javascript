558559282c3d393bc0a54f560bb9194201a9a7b9
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-17 11:11:57 -0300

    Criando arrays

---
+++ algaworks/arrays/criando_arrays.js
@@ -0,0 +1,16 @@
+// definição de um array
+var emails = [];
+
+emails[0] = 'joao@email.com';
+emails[1] = 'maria@email.com';
+
+console.log('email[0]', emails[0]);
+console.log('email[5]', emails[5]);
+
+var nomes = ['João', 'Maria'];
+console.log('Quantos nomes', nomes.length);
+
+nomes[4] = 'José';
+console.log('Quantos nomes depois do josé', nomes.length);
+console.log('nomes[3]', nomes[3]);
+
