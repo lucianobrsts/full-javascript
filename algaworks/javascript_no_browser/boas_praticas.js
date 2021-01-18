f9d1b4dafbb043264d15301a06e9fdef9d269def
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-18 16:00:57 -0300

    Boa práticas

---
+++ algaworks/javascript_no_browser/boas_praticas.js
@@ -0,0 +1,7 @@
+//Como boa prática 1 fazer sempre o html carregar primeiro o estilo e no final o js.
+
+//Como boa prática 2 muito bom sempre encapsular as variáveis dentro de uma função.
+
+//Como boa prática 3 sempre atribuir o valor direto e nunca usar o new para inicializar uma variável ou objeto.
+
+//
