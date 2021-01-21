dd0f10bf90c2d046b8c96b49fca48847a32b53f8
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-21 09:38:52 -0300

    Introdução a protocolo HTTP

---
+++ algaworks/jquery/protocolo_http.js
@@ -0,0 +1,14 @@
+/*conteúdo de um requisição HTTP:
+    - Verbo: GET, POST, PUT, DELETE
+    - URL: http://hostcidades?uf=MG
+    - Headers: Accept, Accept-language, etc
+    - Parâmetros: Query String, Payload
+
+*/
+
+/*conteúdo de um resposta HTTP:
+    - codigo: 200 OK, 404, 500
+    - Headers: Content-Type, etc
+    - Resposta: [{"Codigo" : 1, "nome" : "São Paulo"}, ...]
+
+*/
